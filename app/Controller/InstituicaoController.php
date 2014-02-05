aec08160867bebe8eb5184c0b1ad332bc5803522
Luis Eduardo <eduardo.cfalcao@gmail.com>
2014-02-05 03:20:30 -0200

    inicio da implementacao dos anexos

--- app/Controller/InstituicaoController.php
+++ app/Controller/InstituicaoController.php
@@ -1,6 +1,8 @@
 <?php
 App::uses('AppController', 'Controller');
 App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+App::uses('Folder', 'Utility');  
+App::uses('File', 'Utility');  
 
 class InstituicaoController extends AppController {
 
@@ -63,6 +65,41 @@ class InstituicaoController extends AppController {
 			$this->request->data = $instituicao;
 		}
 	}	
+	
+	public function anexos()
+	{
+		if ($this->request->is('put') || $this->request->is('post')) 
+		{	
+			$files = $this->request->data;
+			//foreach
+			//Ok, com diretório devidamente criado, vamos declarar o arquivo temporário  
+			$arquivo_tmp = new File($arquivo['tmp_name'],false);  
+			  
+			//pegar os dados dele  
+			$dados = $arquivo_tmp->read();  
+			  
+			//e fecha-lo  
+			$arquivo_tmp->close();  
+			  
+			//agora vamosc riar nosso arquivo  
+			$arquivo_nome = new File($diretorio.DS.$arquivo['name'],false,0644);  
+			  
+			//cria-lo  
+			$arquivo_nome->create();  
+			  
+			//escrever os dados armazenados  
+			$arquivo_nome->write($dados);  
+			  
+			//e feixar o arquivo  
+			$arquivo_nome->close();  
+			//$this->Session->setFlash(__('Os arquivos foram salvos.'));
+		}
+		else
+		{
+			$instituicao = $this->Instituicao->findByLogin($this->Auth->user('login'));
+			$this->request->data = $instituicao;
+		}
+	}
 	
 	public function alterarsenha()
 	{
---
+++ app/View/Instituicao/anexos.ctp
@@ -0,0 +1,43 @@
+<?php $this->extend('/layouts/minhacontatemplate'); ?>
+
+
+<?php echo $this->Form->create('Anexo',
+						    array(
+						    		'type' => 'file',
+						    		'url' => array('controller' => 'Instituicao', 'action' => 'anexos'),
+								    'inputDefaults' => array
+								    (
+								        'label' => false,
+								        'div' => false
+								    ) 
+						    	)
+						   ); ?>
+						   
+						   
+	<div class="lar-01">
+		<label for="anexo1"> Anexo 1:</label><br />
+
+        <?php echo $this->Form->file('anexo1'); ?>
+    </div>
+    
+   <div class="lar-01">
+        <label for="nome"> Anexo 2:</label><br />
+
+        <?php echo $this->Form->file('anexo2'); ?>
+   </div>
+    
+    <div class="lar-01">
+        <label for="nome"></label>Anexo 3:</label><br />
+
+        <?php echo $this->Form->file('anexo3'); ?>
+    </div>
+    
+    <div class="lar-01">
+    	<label>Anexo 4:</label><br />
+        <?php echo $this->Form->file('anexo4'); ?>
+    </div>
+    
+    
+<?php echo $this->Form->end("Enviar arquivos"); ?>
+
+<?php print_r($files); ?>
